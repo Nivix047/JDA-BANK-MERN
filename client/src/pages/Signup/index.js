f79f55d7f85bb314b10e6332153c85847ee5588a
Daniel Xu <xudh47@gmail.com>
2022-12-10 17:24:14 -0800

    Updated signup form

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -7,131 +7,165 @@ import { ADD_USER } from "../../utils/mutations";
 import Auth from "../../utils/auth";
 
 const Signup = () => {
-  // const [formState, setFormState] = useState({
-  //   username: "",
-  //   email: "",
-  //   password: "",
-  // });
+  const [formState, setFormState] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+  });
 
-  // const [addUser, { error, data }] = useMutation(ADD_USER);
+  const [addUser, { error, data }] = useMutation(ADD_USER);
 
-  // const handleChange = (event) => {
-  //   const { name, value } = event.target;
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-  //   setFormState({
-  //     ...formState,
-  //     [name]: value,
-  //   });
-  // };
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
 
-  // const handFormSubmit = async (event) => {
-  //   event.preventDefault();
-  //   console.log(formState);
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
 
-  //   try {
-  //     const { data } = await addUser({
-  //       variables: { ...formState },
-  //     });
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
 
-  //     Auth.login(data.addUser.token);
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
   return (
-    <div className="container signup-form">
-      <div className="row">
-        <div className="col-7">
-          <h1 className="display-3">
-            When
-            <span className="highlight"> banking</span>
-            <span> meets</span>
-            <br />
-            <span className="highlight"> minimalist</span>
-            <div className="mt-5 h3">EZ Money EZ Life.</div>
-          </h1>
-        </div>
-        <div className="col">
-          <div className="col d-flex justify-content-center login-signup-form p-4">
-            <form className="signup-form login-signup-form">
-              <div className="row">
-                <div className="col display-6">Welcome!</div>
-                <div className="mb-3">
-                  <label
-                    htmlFor="firstName-signup"
-                    className="form-label"
-                  ></label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="First Name"
-                    id="firstName-signup"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label
-                    htmlFor="lastName-signup"
-                    className="form-label"
-                  ></label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Last Name"
-                    id="lastName-signup"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label
-                    htmlFor="username-signup"
-                    className="form-label"
-                  ></label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Username"
-                    id="username-signup"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="email-signup" className="form-label"></label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    placeholder="Email"
-                    id="email-signup"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label
-                    htmlFor="password-signup"
-                    className="form-label"
-                  ></label>
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Password"
-                    id="password-signup"
-                  />
-                </div>
-                <div className="form-group mb-3">
-                  <button type="submit" className="btn btn-success w-100">
-                    Sign up
-                  </button>
-                </div>
-                <div className="form-group">
-                  <Link
-                    to="/login"
-                    className="text-decoration-none signup-login-href text-success"
-                  >
-                    Login instead
-                  </Link>
-                </div>
+    <main>
+      {data ? (
+        <p>
+          Success! You may now head <Link to="/">back to the homepage.</Link>
+        </p>
+      ) : (
+        <div className="container signup-form">
+          <div className="row">
+            <div className="col-7">
+              <h1 className="display-3">
+                When
+                <span className="highlight"> banking</span>
+                <span> meets</span>
+                <br />
+                <span className="highlight"> minimalist</span>
+                <div className="mt-5 h3">EZ Money EZ Life.</div>
+              </h1>
+            </div>
+            <div className="col">
+              <div className="col d-flex justify-content-center login-signup-form p-4">
+                <form
+                  onSubmit={handleFormSubmit}
+                  className="signup-form login-signup-form"
+                >
+                  <div className="row">
+                    <div className="col display-6">Welcome!</div>
+                    <div className="mb-3">
+                      <label
+                        htmlFor="firstName-signup"
+                        className="form-label"
+                      ></label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="First Name"
+                        id="firstName-signup"
+                        name="firstName"
+                        value={formState.firstName}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label
+                        htmlFor="lastName-signup"
+                        className="form-label"
+                      ></label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Last Name"
+                        id="lastName-signup"
+                        name="lastName"
+                        value={formState.lastName}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label
+                        htmlFor="username-signup"
+                        className="form-label"
+                      ></label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Username"
+                        id="username-signup"
+                        name="username"
+                        value={formState.username}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label
+                        htmlFor="email-signup"
+                        className="form-label"
+                      ></label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                        id="email-signup"
+                        name="email"
+                        value={formState.email}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label
+                        htmlFor="password-signup"
+                        className="form-label"
+                      ></label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Password"
+                        id="password-signup"
+                        name="password"
+                        value={formState.password}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className="form-group mb-3">
+                      <button type="submit" className="btn btn-success w-100">
+                        Sign up
+                      </button>
+                    </div>
+                    <div className="form-group">
+                      <Link
+                        to="/login"
+                        className="text-decoration-none signup-login-href text-success"
+                      >
+                        Login instead
+                      </Link>
+                    </div>
+                  </div>
+                </form>
               </div>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+      {error && (
+        <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+      )}
+    </main>
   );
 };
 
