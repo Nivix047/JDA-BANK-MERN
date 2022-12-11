b9c86fab1099538cf0b25943b300c1dce7334479
Daniel Xu <xudh47@gmail.com>
2022-12-10 22:18:12 -0800

    Updated signed and login form

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -9,81 +9,83 @@ function Navbar() {
     Auth.logout();
   };
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-      <div className="container-fluid">
-        {Auth.loggedIn() ? (
-          <Link className="navbar-brand" id="logout" onClick={logout}>
-            Logout
-          </Link>
-        ) : (
-          <Link to="/login" className="navbar-brand" id="login">
-            Login
-          </Link>
-        )}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            {Auth.loggedIn() ? (
+    <main className="pb-4 pt-4">
+      <nav className="navbar navbar-expand-lg bg-light" id="navbar">
+        <div className="container-fluid">
+          {Auth.loggedIn() ? (
+            <Link className="navbar-brand" id="logout" onClick={logout}>
+              Logout
+            </Link>
+          ) : (
+            <Link to="/login" className="navbar-brand" id="login">
+              Login
+            </Link>
+          )}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              {Auth.loggedIn() ? (
+                <li className="nav-item">
+                  <Link to="/" className="nav-link active" aria-current="page">
+                    Home
+                  </Link>
+                </li>
+              ) : (
+                <></>
+              )}
               <li className="nav-item">
-                <Link to="/" className="nav-link active" aria-current="page">
-                  Home
+                <Link className="nav-link" href="#">
+                  Features
                 </Link>
               </li>
-            ) : (
-              <></>
-            )}
-            <li className="nav-item">
-              <Link className="nav-link" href="#">
-                Features
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" href="#">
-                Pricing
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <Link
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown link
-              </Link>
-              <ul className="dropdown-menu">
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    Action
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    Another action
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" href="#">
-                    Something else here
-                  </Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
+              <li className="nav-item">
+                <Link className="nav-link" href="#">
+                  Pricing
+                </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <Link
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown link
+                </Link>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Action
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Another action
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" href="#">
+                      Something else here
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </main>
   );
 }
 
