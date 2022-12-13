49f39f0243aa7dae021204aa923847c3e9a86488
Daniel Xu <xudh47@gmail.com>
2022-12-12 20:51:09 -0800

    Updated TransactionList to partially render transactions and added getter for dateFormat

--- client/src/components/TransactionList/index.js
+++ client/src/components/TransactionList/index.js
@@ -3,41 +3,87 @@ import React from "react";
 const TransactionList = ({ userInfo }) => {
   console.log("userInfo", userInfo);
   return (
-    <div>
-      {userInfo.transactions.length ? (
-        <div>
-          <div>{userInfo.transactions[0]._id}</div>
-          <div>{userInfo.transactions[0].recipient}</div>
-          <div>{userInfo.transactions[0].transaction_date}</div>
-          <div>{userInfo.transactions[0].message}</div>
+    <div className="col-8 text-center">
+      <div className="col">
+        <div className="m-4">
+          {userInfo.transactions.length ? (
+            <div>
+              {userInfo.transactions.map((transaction) => (
+                <>
+                  <div id="debit-transaction" key={transaction._id}>
+                    You paid {transaction.amount}
+                    <span className="cap"> {transaction.recipient} </span>
+                    on {transaction.transaction_date}
+                  </div>
+                  <div id="credit-transaction" key={transaction._id}>
+                    Paid you
+                    <span className="cap"> {transaction.amount} </span>
+                    on {transaction.transaction_date}
+                  </div>
+                </>
+              ))}
+            </div>
+          ) : (
+            <div>No transactions...</div>
+          )}
         </div>
-      ) : (
-        <div>No transactions...</div>
-      )}
+      </div>
     </div>
-    // <div>
-    //   {userInfo.map((transaction) => (
-    //     <div key={transaction._id}>
-    //       <div>{transaction.transaction_date}</div>
-    //     </div>
-    //   ))}
-    // </div>
-    // <div className="col-8 text-center">
-    //   <div className="col">
-    //     <div className="m-4">
-    //       <div id="debit-transaction">
-    //         You paid
-    //         <span className="cap"> recipient</span>
-    //         on date
-    //       </div>
-    //       <div id="credit-transaction">
-    //         <span className="cap">recipient</span>
-    //         paid you $ credit on date
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
+
+/* <div className="col-8 text-center">
+<div className="col">
+   <div className="m-4">
+    {userInfo.transaction.length ? (
+      <div>
+        {userInfo.transactions.map((transaction) => (
+          <div id="debit-transaction" key={transaction._id}>
+            You paid {transaction.amount}
+            <span className="cap"> {transaction.recipient}</span>
+            on {transaction.transaction_date}
+          </div>
+        ))}
+      </div>
+    ) : (
+      <div>No transactions...</div>
+    )}
+  </div> 
+  
+</div>
+</div> */
+
+//  <div className="col-8 text-center">
+//   <div className="col">
+//     <div className="m-4">
+//       <div id="debit-transaction">
+//         You paid {transaction.recipient}
+//         <span className="cap"> recipient</span>
+//         on {transaction.transaction_date}
+//       </div>
+//       <div id="credit-transaction">
+//         <span className="cap">recipient</span>
+//         paid you $ credit on date
+//       </div>
+//     </div>
+//   </div>
+// </div>;
+
+/* <div>
+{userInfo.transactions.length ? (
+  <div>
+    {userInfo.transactions.map((transaction) => (
+      <div key={transaction._id}>
+        amount: {transaction.amount}
+        transaction date: {transaction.transaction_date}
+        message: {transaction.message}
+        recipient: {transaction.recipient}
+      </div>
+    ))}
+  </div>
+) : (
+  <div>No transactions...</div>
+)}
+</div> */
 
 export default TransactionList;
