d347aeadb184abca1c7a561bbfdb754561385a59
Daniel Xu <xudh47@gmail.com>
2022-12-14 22:03:06 -0800

    Experiting with seeders and working on chartjs

---
+++ server/seeders/seed.js
@@ -0,0 +1,31 @@
+const db = require("../config/connection");
+const { User, Transaction } = require("../models");
+const userSeeds = require("./userSeeds.json");
+const transactionSeeds = require("./transactionSeeds.json");
+
+db.once("open", async () => {
+  try {
+    await Transaction.deleteMany({});
+    await User.deleteMany({});
+
+    await User.create(userSeeds);
+
+    for (let i = 0; i < transactionSeeds.length; i++) {
+      const { _id, sender } = await Transaction.create(transactionSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { username: sender },
+        {
+          $addToSet: {
+            transactions: _id,
+          },
+        }
+      );
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log("all done!");
+  process.exit(0);
+});
