455361b70caf260d357f408c192782b1f30db962
Daniel Xu <xudh47@gmail.com>
2022-12-15 22:19:42 -0800

    Updated routes to render the correct pages

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -66,7 +66,7 @@ const Login = (props) => {
           <div class="spinner-grow text-dark" role="status">
             <span class="visually-hidden">Loading...</span>
           </div>{" "}
-          <Link to="/" />
+          <Link to="/home" />
         </p>
       ) : (
         <div className="container login-form">
