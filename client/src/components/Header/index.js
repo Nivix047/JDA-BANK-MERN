5db82fa46ca7ea21729239bb9e8a8a8e8f3fc7f1
Ben Hobson <benhobson@Bens-MacBook-Pro.local>
2022-12-15 22:26:51 -0800

    fixed header padding

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,7 +2,7 @@ function Header() {
   return (
     <div className="row text-left jumbotron jumbotron-fluid">
       <div className="col container">
-        <h1 className="display-1 title p-5">JDAB Bank</h1>
+        <h1 className="display-1 title">JDAB Bank</h1>
         <p className="lead"></p>
       </div>
     </div>
